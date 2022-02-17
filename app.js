dd297377e8f50e4b73eb977d867a696c66a8aad3
jbrime15 <jubrilibrahim40@gmail.com>
2022-02-17 09:49:13 +0100

    Add mobile menu

---
+++ app.js
@@ -0,0 +1,9 @@
+/* eslint linebreak-style: ["error", "windows"] */
+function myFunction() {
+  const x = document.getElementById('topnav');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
